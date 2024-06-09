b77ce32c129cb0fb4fa045fea8b43dfe2a725702
pete <pete@petervogt.net>
2024-06-09 10:30:07 +0200

    only sync last 2 mod dir

--- .local/bin/syncphone.sh
+++ .local/bin/syncphone.sh
@@ -9,20 +9,27 @@ export TMPDIR=/tmp
 
 #gio mount mtp:host=[device ID]
 #gio mount smb://server/share
+gio mount smb://truenas.local/data/
 
 echo -e "Syncing...."
+
 echo -e "\nFrom: $PC_DIR"
-ls $PC_DIR | wc -l
+ls $PC_DIR | wc -l; echo " folders"
+
 echo -e "\nTo:   $PHONE_DIR"
-ls "$PHONE_DIR" | wc -l
+ls "$PHONE_DIR" | wc -l; echo " folders"
+
 echo -e "\nTemp dir: $TMPDIR\n"
 
 read -n 1 -s -r -p "Press any key to continue..."
 
 echo -e "\nGo...."
 # cp -rvn "$PC_DIR"/* "$PHONE_DIR"
-# rsync -av --progress --iconv=utf-8-mac,utf-8 --chmod=ugo=rwX "$PC_DIR" "$PHONE_DIR"
-rsync -ah --progress --no-times --no-perms --no-group --temp-dir="$TMPDIR"   "$PC_DIR"/* "$PHONE_DIR"/
+# rsync -ah --ignore-existing --progress --no-times --no-perms --no-group --temp-dir="$TMPDIR"   "$PC_DIR"/* "$PHONE_DIR"/
+# find $PC_DIR -type f -mtime -15 -print0 | rsync -ah --ignore-existing --progress --no-times --no-perms --no-group --temp-dir="$TMPDIR" --include-from=- "$PHONE_DIR"/
+
+# Find the two most recently modified subdirectories and sync them
+find "$PC_DIR" -mindepth 1 -maxdepth 1 -type d -printf '%T@ %p\n' | sort -n -r | head -n 2 | cut -d' ' -f2- | xargs -I{} rsync -ahv --ignore-existing --progress --no-times --no-perms --no-group --temp-dir="$TMPDIR" "{}" "$PHONE_DIR"
 
 echo "Done."
 
